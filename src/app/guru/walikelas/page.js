4a93d76f8f77f93647c54e13feb1b588fdfce533
shoofian <wahyushofian@gmail.com>
2026-07-13 19:59:11 +0800

    fix: import Fragment and createPortal in walikelas dashboard

--- src/app/guru/walikelas/page.js
+++ src/app/guru/walikelas/page.js
@@ -1,6 +1,7 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, Fragment } from "react";
+import { createPortal } from "react-dom";
 import { useRouter } from "next/navigation";
 import Link from "next/link";
 
